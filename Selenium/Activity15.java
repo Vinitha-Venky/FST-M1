dfed934cbdb87294817f3d2d9e3e80473ba9d1fb
Vinitha-Venky <Ollala.Vinitha@ibm.com>
2025-01-07 13:53:42 +0530

    Add files via upload

---
+++ Selenium/Activity15.java
@@ -0,0 +1,39 @@
+import java.time.Duration;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class Activity15 {
+    public static void main(String[] args) {
+        
+        WebDriver driver = new FirefoxDriver();
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+        
+        driver.get("https://training-support.net/webelements/dynamic-attributes");
+        
+        System.out.println("Page title is: " + driver.getTitle());
+
+        
+        WebElement fullName = driver.findElement(By.xpath("//input[starts-with(@id, 'full-name')]"));
+        WebElement email = driver.findElement(By.xpath("//input[contains(@id, '-email')]"));
+        WebElement eventDate = driver.findElement(By.xpath("//input[contains(@name, '-event-date-')]"));
+        WebElement details = driver.findElement(By.xpath("//textarea[contains(@id, '-additional-details-')]"));
+        
+        fullName.sendKeys("ollala vinitha");
+        email.sendKeys("kijuy@dhy.com");
+        eventDate.sendKeys("2025-07-26");
+        details.sendKeys("Event By Vinitha!");
+        
+        driver.findElement(By.xpath("//button[text()='Submit']")).click();
+
+        String message = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("action-confirmation")))
+                .getText();
+        System.out.println("Success message: " + message);
+
+        driver.quit();
+    }
+}
